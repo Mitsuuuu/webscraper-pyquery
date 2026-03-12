226c4ab60d59eb0d5cc75923f9c022f672bf8849
ag <alexandergiesecke2002@gmail.com>
2026-03-12 14:02:38 +0100

    Programm sammelt Buchnamen in einer Liste und zeigt Fortschritt als Balken an.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm
+
 import requests
 from pyquery import PyQuery as pq
 
@@ -9,8 +11,23 @@ def getpages():
     pages = pages.split(" of ")
     pages = pages[1]
     return int (pages)
-##  aka: return int(doc("li.current").text().split(" of ")[1])
 
 max_pages = getpages()
+all_books = []
 
-print(max_pages)
+for page in tqdm(range(1, max_pages + 1), desc="Pages:", position=0):
+    url = "https://books.toscrape.com/catalogue/page-{}.html".format(page)
+    r = requests.get(url="https://books.toscrape.com/catalogue/page-{}.html".format(page))
+    doc = pq(r.content)
+    for link in tqdm(doc("h3>a"), desc="Books:",position=1, leave=False):
+        link = "https://books.toscrape.com/catalogue/"+link.attrib["href"]
+        r = requests.get(link)
+        doc = pq(r.content)
+        all_books.append(doc("h1").text())
+print(all_books)
+
+
+
+
+
+
