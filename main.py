a250680b634b980bd5865e9fb1b7f44e3596641a
ag <alexandergiesecke2002@gmail.com>
2026-03-12 17:05:01 +0100

    Anzahl Bücher werden jetzt auch ermittelt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from tqdm import tqdm
-
 import requests
 from pyquery import PyQuery as pq
 
@@ -15,19 +14,23 @@ def getpages():
 max_pages = getpages()
 all_names = []
 anzahl_eintraege = 0
+zahl = int (input("Aktion auswählen:"))
+match zahl:
+    case 1:
+        for page in tqdm(range(1, 2), desc="Pages:", position=0):
+            url = "https://books.toscrape.com/catalogue/page-{}.html".format(page)
+            r = requests.get(url="https://books.toscrape.com/catalogue/page-{}.html".format(page))
+            doc = pq(r.content)
+            for link in tqdm(doc("h3>a"), desc="Books:", position=1, leave=False):
+                link = "https://books.toscrape.com/catalogue/" + link.attrib["href"]
+                r = requests.get(link)
+                doc = pq(r.content)
+                all_names.append(doc("h1").text())
+                anzahl_eintraege += 1
 
-for page in tqdm(range(1, 2), desc="Pages:", position=0):
-    url = "https://books.toscrape.com/catalogue/page-{}.html".format(page)
-    r = requests.get(url="https://books.toscrape.com/catalogue/page-{}.html".format(page))
-    doc = pq(r.content)
-    for link in tqdm(doc("h3>a"), desc="Books:",position=1, leave=False):
-        link = "https://books.toscrape.com/catalogue/"+link.attrib["href"]
-        r = requests.get(link)
-        doc = pq(r.content)
-        all_names.append(doc("h1").text())
-        anzahl_eintraege += 1
-print(all_names)
 print(f"{anzahl_eintraege} Bücher gefunden.")
+print(all_names)
+
 
 
 
